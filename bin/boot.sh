0ec52535720f63a5b1d397c472e1e77b9bfd5b4b
Anton Vasilyev <antonvas@gmail.com>
2023-02-20 15:11:23 +0000

    Use loop

--- bin/boot.sh
+++ bin/boot.sh
@@ -4,38 +4,24 @@ echo 'Loading...'
 
 mkdir -p /app/models
 
-if ! test -f /app/models/model_large_caption.pth
-then
-  curl -L https://storage.googleapis.com/sfr-vision-language-research/BLIP/models/model_large_caption.pth --output /app/models/model_large_caption.pth
-fi
-if ! test -f /app/models/ViT-L-14.pt
-then
-  curl -L https://openaipublic.azureedge.net/clip/models/b8cca3fd41ae0c99ba7e8951adf17d267cdb84cd88be6f7c2e0eca1737a03836/ViT-L-14.pt --output /app/models/ViT-L-14.pt
-fi
-if ! test -f /app/models/ViT-L-14_openai_artists.safetensors
-then
-  curl -L https://huggingface.co/pharma/ci-preprocess/resolve/main/ViT-L-14_openai_artists.safetensors --output /app/models/ViT-L-14_openai_artists.safetensors
-fi
-if ! test -f /app/models/ViT-L-14_openai_flavors.safetensors
-then
-  curl -L https://huggingface.co/pharma/ci-preprocess/resolve/main/ViT-L-14_openai_flavors.safetensors --output /app/models/ViT-L-14_openai_flavors.safetensors
-fi
-if ! test -f /app/models/ViT-L-14_openai_mediums.safetensors
-then
-  curl -L https://huggingface.co/pharma/ci-preprocess/resolve/main/ViT-L-14_openai_mediums.safetensors --output /app/models/ViT-L-14_openai_mediums.safetensors
-fi
-if ! test -f /app/models/ViT-L-14_openai_movements.safetensors
-then
-  curl -L https://huggingface.co/pharma/ci-preprocess/resolve/main/ViT-L-14_openai_movements.safetensors --output /app/models/ViT-L-14_openai_movements.safetensors
-fi
-if ! test -f /app/models/ViT-L-14_openai_trendings.safetensors
-then
-  curl -L https://huggingface.co/pharma/ci-preprocess/resolve/main/ViT-L-14_openai_trendings.safetensors --output /app/models/ViT-L-14_openai_trendings.safetensors
-fi
-if ! test -f /app/models/ViT-L-14_openai_negative.safetensors
-then
-  curl -L https://huggingface.co/pharma/ci-preprocess/resolve/main/ViT-L-14_openai_negative.safetensors --output /app/models/ViT-L-14_openai_negative.safetensors
-fi
+declare -A models
+models=(
+  ["model_large_caption.pth"]="https://storage.googleapis.com/sfr-vision-language-research/BLIP/models/"
+  ["ViT-L-14.pt"]="https://openaipublic.azureedge.net/clip/models/b8cca3fd41ae0c99ba7e8951adf17d267cdb84cd88be6f7c2e0eca1737a03836/"
+  ["ViT-L-14_openai_artists.safetensors"]="https://huggingface.co/pharma/ci-preprocess/resolve/main/"
+  ["ViT-L-14_openai_flavors.safetensors"]="https://huggingface.co/pharma/ci-preprocess/resolve/main/"
+  ["ViT-L-14_openai_mediums.safetensors"]="https://huggingface.co/pharma/ci-preprocess/resolve/main/"
+  ["ViT-L-14_openai_movements.safetensors"]="https://huggingface.co/pharma/ci-preprocess/resolve/main/"
+  ["ViT-L-14_openai_trendings.safetensors"]="https://huggingface.co/pharma/ci-preprocess/resolve/main/"
+  ["ViT-L-14_openai_negative.safetensors"]="https://huggingface.co/pharma/ci-preprocess/resolve/main/"
+)
+
+for file in "${!models[@]}"; do
+  if ! test -f /app/models/$file
+  then
+    curl -L ${models[$file]}$file --output /app/models/$file
+  fi
+done
 
 
 gunicorn --reload --log-level debug --bind 0.0.0.0:80 --timeout 120  --pythonpath '/app' main:app --workers 1 --worker-class eventlet
